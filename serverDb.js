b7bcdc378b4f287faa405578fe69750a187dfa21
Brambilla05 <brambillagianluca@itis-molinari.eu>
2025-02-11 10:18:48 +0100

    gestione lato server finita

--- serverDb.js
+++ serverDb.js
@@ -1,130 +1,48 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const path = require("path");
-const http = require("http");
-
-const fs = require('fs');
 const mysql = require('mysql2');
-const conf = JSON.parse(fs.readFileSync('conf.json'));
-conf.ssl.ca = fs.readFileSync(__dirname + '/ca.pem');
 const connection = mysql.createConnection(conf);
 
-const app = express();
-
-
-app.use(bodyParser.json());
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
-app.use("/", express.static(path.join(__dirname, "public")));
-
-app.post("/todo/add", async (req, res) => {
-  const todo = req.body.todo;
-
-  await insert(todo);
-
-  res.json({ result: "Ok" });
-});
-
-app.get("/todo", async (req, res) => {
-  const todo = await select()
-  res.json({ todos: todo });
-});
-
-app.put("/todo/complete", async (req, res) => {
-  const todo = req.body;
-
-  try {
-    let todos = await select()
-
-    await todos.map(async (element) => {
-      if (element.id === todo.id) {
-        element.completed = true;
-        await update(element);
-      }
-      return element;
-    });
-
-  } catch (e) {
-    console.log(e);
-  }
-  res.json({ result: "Ok" });
-});
-
-app.delete("/todo/:id", async (req, res) => {
-  let todos = await select()
-  todos = todos.filter((element) => element.id == req.params.id);
-  await remove(todos[0]);
-
-  res.json({ result: "Ok" });
-});
-
-const executeQuery = (sql) => {
+const executeQuery = (sql, params = []) => {
   return new Promise((resolve, reject) => {
-    connection.query(sql, function (err, result) {
+    connection.query(sql, params, (err, result) => {
       if (err) {
         console.error(err);
-        reject();
+        return reject(err);
       }
-      console.log('done');
       resolve(result);
     });
-  })
-}
+  });
+};
 
-const createTable = async () => {
-  return await executeQuery(`
-    CREATE TABLE IF NOT EXISTS todo
-       ( id INT PRIMARY KEY AUTO_INCREMENT, 
-          name VARCHAR(255) NOT NULL, 
-          completed BOOLEAN ) 
-       `);
-}
+const db = {
+  createTable: async () => {
+    return await executeQuery(`
+      CREATE TABLE IF NOT EXISTS images (
+        id INT PRIMARY KEY AUTO_INCREMENT,  
+        url VARCHAR(255) NOT NULL
+      );
+    `);
+  },
 
-const insert = async (todo) => {
-  const template = `
-    INSERT INTO todo (name, completed) VALUES ('$NAME', '$COMPLETED')
-       `;
-  let sql = template.replace("$NAME", todo.name);
-  sql = sql.replace("$COMPLETED", todo.completed ? 1 : 0);
-  return await executeQuery(sql);
-}
+  insert: async (img) => {
+    const sql = `INSERT INTO images (url) VALUES (?)`;
+    return await executeQuery(sql, [img.url]);
+  },
+  
+  select: async () => {
+    return await executeQuery(`SELECT id, url FROM images`);
+  },
 
-const select = async () => {
-  const sql = `
-    SELECT id, name, completed FROM todo 
-       `;
-  return await executeQuery(sql);
-}
+  update: async (img) => {
+    const sql = `UPDATE images SET url = ? WHERE id = ?`;
+    return await executeQuery(sql, [img.url, img.id]);
+  },
 
-const update = async (todo) => {
-  let sql = `
-    UPDATE todo SET completed = '$COMPLETED' WHERE id = '$ID'
-    `;
+  remove: async (img) => {
+    const sql = `DELETE FROM images WHERE id = ?`;
+    return await executeQuery(sql, [img.id]);
+  }
+};
 
-  sql = sql.replace("$COMPLETED", todo.completed ? 1 : 0);
-  sql = sql.replace("$ID", todo.id);
-  return await executeQuery(sql);
-}
+module.exports = db;
 
 
-const remove = async (todo) => {
-  let sql = `
-  DELETE FROM todo WHERE id = '$ID'
-  `;
-
-  sql = sql.replace("$ID", todo.id);
-  return await executeQuery(sql);
-}
-
-
-createTable().then(() => {
-  const server = http.createServer(app);
-
-  server.listen(80, () => {
-    console.log("- server running");
-  })
-}
-);
--- serverUpload.js
+++ serverUpload.js
@@ -1,9 +1,12 @@
 const express = require("express");
-const http = require('http');
-const path = require('path');
+const http = require("http");
+const path = require("path");
+const multer = require("multer");
+const fs = require("fs");
+const bodyParser = require("body-parser");
+const database = require("./serverDb.js");
+
 const app = express();
-const multer  = require('multer');
-const fs = require('fs');
 
 let storage = multer.diskStorage({
     destination: function (req, file, callback) {
@@ -13,27 +16,33 @@ let storage = multer.diskStorage({
         callback(null, file.originalname);
     }
 });
-const upload = multer({ storage: storage}).single('file');
 
+const upload = multer({ storage: storage }).single("file");
 
 app.use("/", express.static(path.join(__dirname, "public")));
 app.use("/files", express.static(path.join(__dirname, "files")));
-app.get("/filelist", (req, res) => {
-    let road = fs.readdirSync(path.join(__dirname, "files"));
-road = road.map(file => {
-  return "./files/" + file; 
-});
+
+app.get("/get", async (req, res) => {
+    let road = await database.select();
     res.json(road);
 });
-app.post('/upload', (req, res) => {
-    upload(req, res, (err) => {
 
-    console.log(req.file.filename);    
-    res.json({url: "./files/" + req.file.filename});    
-  })
+app.post("/add", async (req, res) => {
+    await upload(req, res, async (err) => {
+        let road = await database.insert();
+        console.log(req.file.filename);
+        res.json({ url: "./files/" + req.file.filename });
+    });
 });
 
+app.delete("/delete/:id", async function (req, res) {
+    const files = await database.select();
+    const toDelete = files.find(function (file) { return file.id == req.params.id; });
+        await database.delete(toDelete.id);
+        res.json({ result: "Ok" });
+    });
+
 const server = http.createServer(app);
-server.listen(5600, () => {
-  console.log("- server running");
+server.listen(80, () => {
+    console.log("- server running");
 });
