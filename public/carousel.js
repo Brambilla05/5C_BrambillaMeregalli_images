6f2f8aea9a2f31573d73bc4f0c4899ad59d18957
Brambilla05 <brambillagianluca@itis-molinari.eu>
2025-02-15 19:38:31 +0100

    continuo  parte grafica

--- public/carousel.js
+++ public/carousel.js
@@ -1,37 +1,36 @@
-export const carousel = (parentElement,pubsub) => {
+export const carousel = (parentElement, pubsub) => {
     let images;
 
     return {
-        build:function(img){
-           images=img;
-           pubsub.subscribe("imagesAdd", (data)=>{
-            
-            images=data;
-            this.render();
-        })
-        pubsub.subscribe("reload", (data)=>{
-            images=data;
-            this.render();
-        })
+        build: function (img) {
+            images = img;
+            pubsub.subscribe("imagesAdd", (data) => {
+                images = data;
+                this.render();
+            });
+            pubsub.subscribe("reload", (data) => {
+                images = data;
+                this.render();
+            });
         },
-        render:function(){
+        render: function () {
             let html = "";
             images.forEach((element, i) => {
-                if(i==0){
+                if (i == 0) {
                     html += `
                     <div class="carousel-item active">
-                    <img src="${element.url}" class="d-block w-100" alt="...">
+                    <img src="${element.url}" class="d-block">
                     </div>
-                    `
-                }else{
+                    `;
+                } else {
                     html += `
                     <div class="carousel-item">
-                    <img src="${element.url}" class="d-block w-100" alt="...">
+                    <img src="${element.url}" class="d-block" >
                     </div>
-                    `
+                    `;
                 }
             });
             parentElement.innerHTML = html;
         }
-    }
-}
+    };
+};
--- public/todo.js
+++ public/todo.js
@@ -1,98 +1,88 @@
+export const generateTodo = function (parentElement, pubsub) {
+  let data = [];
 
-
-export const generateTodo = function (parentElement,pubsub) {
-    let data = [];
-    
-    return {
+  return {
       send: (todo) => {
-        return new Promise((resolve, reject) => {
-          fetch("/todo/add", {
-            method: "POST",
-  
-            headers: {
-              "Content-Type": "application/json",
-            },
-  
-            body: JSON.stringify(todo),
-          })
-            .then((response) => response.json())
-  
-            .then((json) => {
-              resolve(json); 
-            });
-        });
-      },
-  
-      load: () => {
-        return new Promise((resolve, reject) => {
-          fetch("/get")
-            .then((response) => response.json())
-  
-            .then((json) => {
-              resolve(json);
-            });
-        });
-      },
-  
-  
-      deleteTodo: (id) => {
-        return new Promise((resolve, reject) => {
-          fetch("/delete/" + id, {
-            method: "DELETE",
-  
-            headers: {
-              "Content-Type": "application/json",
-            },
-          })
-            .then((response) => response.json())
-  
-            .then((json) => {
-              resolve(json);
-            });
-        });
-      },
-  
-      render: function () {
-        pubsub.subscribe("reload",(d)=>{
-      data=d;
-      this.render();
-        });
-        let html = '<table class="table table-striped"><tbody>';
-        data.forEach((e, index) => {
-          let cssClass = e.completed ? "task-completed " : "";
-          console.log(e.url);
-          html +=
-            "<tr class=" +
-            cssClass +
-            "table-row" +
-            ">" +
-            "<td>" +
-            "<img src='"+
-            e.url +
-            "' alt='mere'></td>" +
-            '<td><button type="button" class="btn btn-danger" id="' +
-            index +
-            '">DELETE</button></td>' +
-            "</tr>";
-        });
-        html += "</tbody></table>";
-        console.log(html);
-        parentElement.innerHTML = html;
-        document.querySelectorAll("button.btn-danger").forEach((e, index) => {
-          e.onclick = () => {
-            console.log(e)
-            this.deleteTodo(data[index].id).then(() => {
-              this.load().then((r) => {
-                this.setData(r);
-                pubsub.publish("imagesAdd",r);
-                this.render();
+          return new Promise((resolve, reject) => {
+              fetch("/todo/add", {
+                  method: "POST",
+                  headers: {
+                      "Content-Type": "application/json",
+                  },
+                  body: JSON.stringify(todo),
+              })
+              .then((response) => response.json())
+              .then((json) => {
+                  resolve(json);
               });
-            });
-          };
-        });
+          });
       },
+
+      load: () => {
+          return new Promise((resolve, reject) => {
+              fetch("/get")
+              .then((response) => response.json())
+              .then((json) => {
+                  resolve(json);
+              });
+});
+      },
+
+      deleteTodo: (id) => {
+          return new Promise((resolve, reject) => {
+              fetch("/delete/" + id, {
+                  method: "DELETE",
+                  headers: {
+                      "Content-Type": "application/json",
+                  },
+              })
+              .then((response) => response.json())
+              .then((json) => {
+                  resolve(json);
+              });
+          });
+      },
+
+      render: function () {
+          pubsub.subscribe("reload", (d) => {
+              data = d;
+              this.render();
+          });
+          let html = '<table class="table table-striped"><tbody>';
+          data.forEach((e, index) => {
+              let cssClass = e.completed ? "task-completed " : "";
+              html +=
+                  "<tr class=" +
+                  cssClass +
+                  "table-row" +
+                  ">" +
+                  "<td>" +
+                  "<img src='" +
+                  e.url +
+                  "' alt='Image' class='img-fluid'>" + // Add class for responsive image
+                  "</td>" +
+                  '<td><button type="button" class="btn btn-danger" id="' +
+                  index +
+                  '">DELETE</button></td>' +
+                  "</tr>";
+          });
+          html += "</tbody></table>";
+          parentElement.innerHTML = html;
+          document.querySelectorAll("button.btn-danger").forEach((e, index) => {
+              e.onclick = () => {
+                  this.deleteTodo(data[index].id).then(() => {
+                      this.load().then((r) => {
+                          this.setData(r);
+                          pubsub.publish("imagesAdd", r);
+                          this.render();
+                      });
+                  });
+              };
+          });
+      },
+
       setData: (newValue) => {
-        data = newValue;
+          data = newValue;
       },
-    };
   };
+};
